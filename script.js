6fb8228a632f58a1cb39bd445d6bdd084fe49dc1
inclined4X <jezemiahsam48@gmail.com>
2024-01-20 23:25:53 +0000

    for service rating when any of the option value is chosen

--- script.js
+++ script.js
@@ -15,11 +15,45 @@ function calculateTip(){
     const amountPerPersonOutput = document.getElementById('amountPerPerson');
 
 
-    // changes that has to do wwith cconverting strings to numbers etc
+    // changes that has to do with cconverting strings to numbers etc
     const billAmount = parseFloat(billAmountInput.value);
     const serviceRating = parseFloat(serviceRatingSelect.value);
     const splitCount = parseInt(splitCountInput.value);
-    const mealType  = mealTypeSelect
+    const mealType  = mealTypeSelect.value;
+
+
+    //conditions for empty inputs
+    if(isNaN(billAmount) || isNaN(serviceRating) || isNaN(splitCount)){
+        tipAmountOutput.textContent = "Please Enter Valid Inputs";
+        totalAmountOutput.textContent = "";
+        amountPerPersonOutput.textContent = "";
+        return;
+    }
+
+
+//  for service rating when any of the option value is chosen
+    let tip;
+    switch(serviceRating){
+        // case 1
+        case 1:
+            tip = billAmount * 0.05;
+            break;
+        // Case 2
+        case 2:
+            tip = billAmount * 0.10;
+            break;
+        // Case 3
+        case 3:
+            tip = billAmount * 0.15;
+            break;
+        // case 4
+        case 4:
+            tip = billAmount * 0.20;
+            break;
+    }
+
+    let totalAmount = billAmount + tip;
+    let amountPerPerson =  totalAmount / splitCount;
 
 
 
