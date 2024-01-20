80bac635e4f554b5625e8bd34b45c71ff307c12a
inclined4X <jezemiahsam48@gmail.com>
2024-01-20 23:02:27 +0000

    got all the IDs of the empty divs which displays the results and also changed one of the id to fit camel case

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function calculateTip(){
 
+
+
+    //getting the elements of all the inputs
     const billAmountInput = document.getElementById('billAmount');
     const serviceRatingSelect = document.getElementById('ServiceRating');
-    const splitCountType = document.getElementById('splitCount');
+    const splitCountInput = document.getElementById('splitCount');
+    const mealTypeInput = document.getElementById('mealType');
+
+
+    // getting the IDs of the empty divs which displays the results
+    const tipAmountOutput = document.getElementById('tipAmount');
+    const tipAmountOutput = document.getElementById('totalAmount');
+    const tipAmountOutput = document.getElementById('AmountPerPerson');
 
 
 
