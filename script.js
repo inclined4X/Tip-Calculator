dcbb9383689a45595f8bf40846724ae1670bc100
inclined4X <jezemiahsam48@gmail.com>
2024-01-20 22:56:22 +0000

    did get all input elements that are going to take some claculations for the tip calculator to work

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculateTip(){
+
+    const billAmountInput = document.getElementById('billAmount');
+    const serviceRatingSelect = document.getElementById('ServiceRating');
+    const splitCountType = document.getElementById('splitCount');
+
+
+
+
+
+
+
+}
+
+
+const calculateBtn = document.getElementById('calculateBtn').addEventListener('click', calculateTip);
